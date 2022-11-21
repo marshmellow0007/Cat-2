587c7e32ae242125d8dcad4ea3df40b2fba3c733
paul mungai <97960418+marshmellow0007@users.noreply.github.com>
2022-11-21 20:16:43 +0300

    Add files via upload

---
+++ cat 2 section B.java
@@ -0,0 +1,63 @@
+// importing the File class
+import java.io.File;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create a file object for the current location
+    File file = new File("JavaFile.java");
+
+public class Geometry
+{
+public static void main(String[] args)
+{
+int , double, char ;      
+     
+double radius;   
+// The radius of the circle      
+double length;   
+// The length of the rectangle      
+double width;   
+// The width of the rectangle   
+double length;
+// The length of the Box   
+double width;
+// The width of the Box   
+double height;
+// The height of the Box  
+double radius;
+// The radius of the sphere    
+
+{            
+case 1:
+print("Enter the radius of " + "the circle: ");               
+radius = keyboard.nextDouble();              
+break;            
+case 2:
+print("Enter the length of " + "the rectangle: ");               
+length = keyboard.nextDouble();               
+print("Enter the width of " +   "the rectangle: ");               
+width = keyboard.nextDouble();              
+break;                      
+case 3:
+print("Enter the radius of " + "the circle: ");               
+radius = keyboard.nextDouble();              
+break;            
+case 4:
+print("Enter the length of " + "the rectangle: ");               
+length = keyboard.nextDouble();               
+print("Enter the width of " +    "the rectangle: ");               
+width = keyboard.nextDouble();            
+break;            
+case 5:
+print("Enter the length of " + "the box: ");
+length = keyboard.nextDouble();
+print("Enter the width of " + "the box: ");
+width = keyboard.nextDouble();
+print("Enter the height of " + "the box: ");
+height = keyboard.nextDouble();
+print("Enter the radius of " + "the sphere: ");               
+radius = keyboard.nextDouble();  
+}
+
+}
